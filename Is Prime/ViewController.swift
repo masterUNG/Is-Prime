9a023cb2f472ef25b0094fca70c79f8aac976c5d
MasterUNG <phrombutr@gmail.com>
2017-07-23 03:06:13 +0700

    Project Success

--- Is Prime/ViewController.swift
+++ Is Prime/ViewController.swift
@@ -9,6 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var showResultLabel: UILabel!
+    
+    
+    @IBOutlet weak var numberTextField: UITextField!
+    
+    
+    @IBAction func checkButton(_ sender: Any) {
+        
+        if let strNumber = numberTextField.text {
+            let intNumber = Int(strNumber)
+            
+            if intNumber != nil {
+                
+                var intI = 2
+                var bolPrime = true
+                
+                while intI < intNumber! {
+                    
+                    if intNumber! % intI == 0 {
+                        bolPrime = false
+                    }
+                    
+                    intI += 1
+                }
+                
+                if bolPrime {
+                    showResultLabel.text = "It Prime"
+                }   else {
+                    showResultLabel.text = "Not Prime"
+                }
+                
+                
+            }   else {
+            
+                showResultLabel.text = "Please Fill Number"
+                
+            }
+            
+        }
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
